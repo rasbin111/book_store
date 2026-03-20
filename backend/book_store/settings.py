3c1732c984d7aa23356e88b1f1e77608291c4fff
rgt <rasbingodarthapa@gmail.com>
2026-03-20 15:23:17 +0545

    update minor code

--- backend/book_store/settings.py
+++ backend/book_store/settings.py
@@ -90,17 +90,24 @@ WSGI_APPLICATION = 'book_store.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+use_sqlite = os.getenv("USE_SQLITE", False)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
+if use_sqlite:
+    DATABASES = {
+        "default": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "dq.sqlite3"
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
+        }
+    }
 
 
 # Password validation
